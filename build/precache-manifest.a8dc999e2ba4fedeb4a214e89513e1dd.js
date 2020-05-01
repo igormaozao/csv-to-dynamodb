f5ce577e391650ce02c75dcdf500a88744b25c20
Igor Mathias <igormaozao@gmail.com>
2020-05-01 13:56:47 -0300

    Adding build folder

---
+++ build/precache-manifest.a8dc999e2ba4fedeb4a214e89513e1dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f623b57bd3115806e6a8731ab451071",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c49bd0b2e80b8f41b35",
+    "url": "/static/css/main.1a362739.chunk.css"
+  },
+  {
+    "revision": "a227be3c727e2f728a8a",
+    "url": "/static/js/2.891e3306.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.891e3306.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c49bd0b2e80b8f41b35",
+    "url": "/static/js/main.a990f196.chunk.js"
+  },
+  {
+    "revision": "b657f90ac2d0dbbd0309",
+    "url": "/static/js/runtime-main.e10742b5.js"
+  }
+]);
