164fa2658ff213e60f0784062253d6ab7aac23cd
Igor Mathias <igormaozao@gmail.com>
2020-06-19 00:00:30 -0300

    Update - Download JSON file

---
+++ precache-manifest.3a02f046c7544839b8aeb550f4489ffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47affc61317c8ccb45dda877cefb8cdd",
+    "url": "/csv-to-dynamodb/index.html"
+  },
+  {
+    "revision": "5c900b3bb59e44ac8c71",
+    "url": "/csv-to-dynamodb/static/css/main.62efb059.chunk.css"
+  },
+  {
+    "revision": "8a7f4dc6dd123b6a9bd4",
+    "url": "/csv-to-dynamodb/static/js/2.9b265ac4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/csv-to-dynamodb/static/js/2.9b265ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c900b3bb59e44ac8c71",
+    "url": "/csv-to-dynamodb/static/js/main.a2f9eddc.chunk.js"
+  },
+  {
+    "revision": "3ad3898fb774fbb1575f",
+    "url": "/csv-to-dynamodb/static/js/runtime-main.75e518b0.js"
+  }
+]);
--- src/model/Header.tsx
+++
@@ -1,4 +0,0 @@
-export default interface Header {
-  name: string;
-  type: string;
-}
